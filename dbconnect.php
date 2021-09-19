8e32451c1413cc81b893715f09c77575176742ca
marutang <66076249+marutang@users.noreply.github.com>
2021-09-19 10:01:41 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
     //定义连接数据库参数
-    define('db_host','localhost');
-    define('db_user','oasis');
-    define('db_password','456123');
-    define('db_name','oasis');
+    define('db_host','');
+    define('db_user','');
+    define('db_password','');
+    define('db_name','');
     //连接数据库
     $dbc = mysqli_connect(db_host,db_user,db_password,db_name) or die('连接数据库失败');
 ?>
